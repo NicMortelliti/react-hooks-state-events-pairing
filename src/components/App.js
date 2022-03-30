b5984180524d2b499fbb6e13a62fd69cd916a2a8
NicMortelliti <npmortelliti@gmail.com>
2022-03-29 19:56:32 -0700

    Remove unused state storage from app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import video from "../data/video.js";
 import Iframe from "./Iframe.js";
 import Title from "./Title.js";
@@ -9,17 +9,13 @@ import CommentStack from "./CommentStack.js";
 function App() {
   console.log("Here's your data:", video);
 
-  // Set up states
-  const [viewsState, setViewsState] = useState(video.views);
-  const [commentsArrayState, setCommentsArrayState] = useState(video.comments);
-
   return (
     <div className="App">
       <Iframe video={video} />
       <Title videoTitle={video.title} />
-      <Stats views={viewsState} videoUploadDate={video.createdAt} />
+      <Stats views={video.views} videoUploadDate={video.createdAt} />
       <Votes upVotes={video.upvotes} downVotes={video.downvotes} />
-      <CommentStack comments={commentsArrayState} />
+      <CommentStack comments={video.comments} />
     </div>
   );
 }
